1196bebff34f3c5ff891213e1c89627ff29f246a
HaseBaldrian <sonnengeist55@gmx.de>
2013-05-29 10:56:49 +0200

    options bei answers werden richtig angezeigt mit newline :)

--- app/models/opt_answer.rb
+++ app/models/opt_answer.rb
@@ -1,2 +1,14 @@
 class OptAnswer < Answer
+  
+  def to_html
+    input_new = ""
+    if self.input
+      self.input.each_line do |l|
+        unless l.strip.blank? # heißt: ist leerzeile
+          input_new += l.strip + "<br />"
+        end
+      end
+    end
+    return input_new
+  end
 end
