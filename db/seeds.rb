fca54e3b729ec8a638f2f1fce10b1cc9e24269cb
HaseBaldrian <sonnengeist55@gmx.de>
2013-06-01 12:18:41 +0200

    seeds.rb legt zu beginn den ersten user (=superuser) an

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create({"name" => "superuser", "email" => "superuser@default.org", "password" => "test", "password_confirmation" => "test"})
